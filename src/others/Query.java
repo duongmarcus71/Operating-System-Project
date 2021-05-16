9b1d10a609e307cc2ea2fac35b0fabac83f1361c
Mas Mas <37113329+duongmarcus71@users.noreply.github.com>
2021-05-16 09:51:56 +0700

    Update Query.java

--- src/others/Query.java
+++ src/others/Query.java
@@ -1,27 +1,48 @@
 package others;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.Vector;
-
+import element.*;
+import element.Process;
 public class Query {
 	
 	private int posProcess;
-	
 	private Vector<Integer> request;
+	private Map<String, Integer> queryMap;
 	
-	public Query(int n,Vector<Integer> max) {
+	public Query(int n, Coordinator c) {
 		
 		Random rd = new Random();
 		
 		posProcess = rd.nextInt(n);
+		Process p = c.getProcess().elementAt(posProcess);
+		Vector<Integer> max = p.getMax();
+		
 		request = new Vector<Integer>(max.size());
+		queryMap = new HashMap<>(max.size());
+		
 		for(int i = 0; i < max.size(); ++ i) {
-			request.add(rd.nextInt(max.get(i)));
+			int random = rd.nextInt(max.get(i));
+			request.add(random);
+			queryMap.put(c.getResource().elementAt(i).getName(), random);
 		}
+		
+		
 	}
 	
 	int getPos() {
 		return posProcess;
 	}
 
+	public Map<String, Integer> getQueryMap(){
+		return queryMap;
+	}
+	
+	public void printQuery() {
+		for(String name : queryMap.keySet()) {
+			System.out.println(name + " - " + queryMap.get(name));
+		}
+	}
 }
